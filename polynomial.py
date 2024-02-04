725b9118f6b7923a72a0c1c6702fe3a57209fb09
Alvis Zou <alviszou@bu.edu>
2024-02-04 01:53:54 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -12,6 +15,9 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, x):
+        return self.i
+    
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -19,7 +25,10 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
-
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+    
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -33,7 +42,10 @@ class Mul:
         if isinstance(self.p2, (Add, Sub, Div, Mul)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
-
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1;
@@ -47,6 +59,9 @@ class Sub:
             return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1;
@@ -59,3 +74,8 @@ class Div:
         if isinstance(self.p2, (Add, Sub, Mul, Div)):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
+    
+    def evaluate(self, x):
+        if self.p2.evaluate(x) == 0:
+            raise ValueError("Cannot divide by 0")
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
